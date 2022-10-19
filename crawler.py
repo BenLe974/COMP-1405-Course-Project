d3c5d7fb879418727e036724a5c71399b9b2fae8
BenLe974 <ben.le974@gmail.com>
2022-10-18 22:45:42 -0400

    beginning of creation of searchdata.py
    refined crawler and created searchdata.py

--- crawler.py
+++ crawler.py
@@ -1,8 +1,14 @@
 import parse_functions
 import os_functions
 import os
+import webdev
 
 def crawl(seed): #seed is url that you start from
+    if webdev.read_url(seed) == "":
+        return print("This is not a valid URL")
+        
+    if os_functions.directory_check("search_results") == False:
+        os_functions.directory_gen("search_results")
     
     directories = os.listdir("search_results")
     for i in directories:
@@ -23,9 +29,20 @@ def crawl(seed): #seed is url that you start from
         url = queue.pop()
         title = parse_functions.find_title(url)
         contents = parse_functions.page_contents(url)
-        os_functions.directory_gen(os.path.join("search_results",title))
+        filepath = os.path.join("search_results",title)
+        os_functions.directory_gen(filepath)
+        os_functions.file_gen(filepath,"page_address.txt",url)
         for key in contents:
-            os_functions.file_gen(os.path.join("search_results",title),key,contents[key])
+            os_functions.file_gen(filepath,key + ".txt",contents[key])
+        references = parse_functions.reference_gen(url)
+        
+        for link in references:
+            if not os.path.exists(os.path.join(filepath,"outgoing_links.txt")):
+                os_functions.file_gen(filepath,"outgoing_links.txt",link)
+            else:
+                filein = open(os.path.join(filepath,"outgoing_links.txt"),"a")
+                filein.write(link + "\n")
+                filein.close()
         totalpages += 1
     return totalpages
 
